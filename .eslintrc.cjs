1265b7e3a728bb76258f6c88b2e907287ef16171
Georgina Neilson <georgina.neilson@prettylittlething.com>
2025-06-03 09:42:18 +0100

    fix linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/* eslint-env node */
+
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: [
+    ".eslintrc.cjs",
+    "vitest.config.ts",
+    "vitest.setup.ts",
+    "lint-staged.config.cjs ",
+  ],
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
